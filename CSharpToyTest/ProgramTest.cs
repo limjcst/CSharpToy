71928f21951d714413fa2cbfd998f14094d450ba
limjcst <limjcst@163.com>
2018-07-19 15:28:59 +0800

    Enable unit test with nunit and nuget package manager
    Signed-off-by: limjcst <limjcst@163.com>

---
+++ CSharpToyTest/ProgramTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+using CSharpToy;
+
+namespace CSharpToyTest
+{
+
+    [TestFixture]
+    public class ProgramTest
+    {
+        private Program program;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            program = new Program();
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            Assert.AreEqual(2, program.Add(1, 1));
+        }
+    }
+}
